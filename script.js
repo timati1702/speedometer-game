47b1e205fdb172c26094681765e521bf29bacd37
Студент <student@NLA.LOCAL>
2025-06-27 16:43:38 +0300

    feat: add load-spinner

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-import {AudioPanel} from './resources/modules/audio_panel.js';
+import { AudioPanel } from './resources/modules/audio_panel.js';
 import { CarPanel } from './resources/modules/car_panel.js';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    const load = document.getElementById('loading-indicator');
+    load.classList.add('active');
+    setTimeout(
+        () => {
+            load.classList.remove('active');
+        },
+        5 * 1000)
+
     const tracks = document.querySelectorAll('.track');
     const player = new AudioPanel();
     player.render
